d8ea9cbbe76fb27d53c17760105f5a835a83f240
sakumashirayuki <2909549511@qq.com>
2021-03-23 22:47:13 +0800

    add communications with fake back-end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,48 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import CreateArea from "./CreateArea"
+import NoTaskHeader from "./NoTaskHeader";
+import CreateArea from "./CreateArea";
 
-import notes from "../notes";
+// import notes from "../notes";
 
 function App() {
-  const [notesArray, setNotesArray] = React.useState(notes);
+  const [notesArray, setNotesArray] = React.useState([]);
+  React.useEffect(()=>{
+    const getNotes = async()=>{
+      const notesFromServer = await fetchNotes();
+      setNotesArray(notesFromServer);
+    };
+    getNotes();
+  }, []);
+// Fetch notes
+  const fetchNotes = async ()=>{
+    const res = await fetch('http://localhost:5000/notes');// This is just a HTTP response
+    const data = await res.json();
 
-  function addNote(newNote){
-    setNotesArray((prevArray)=>{
-      return [...prevArray, newNote];
-    })
-    console.log(notesArray);
+    return(data);
+  }
+  async function addNote(newNote) {
+    const res = await fetch('http://localhost:5000/notes',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newNote)
+    });
+    const data = await res.json(); // the data is the new note that's added to the server
+    setNotesArray((prevArray) => {
+      return [...prevArray, data];
+    });
   }
 
-  function deleteNote(noteId){
-    setNotesArray((prevArray)=>{
-      return prevArray.filter((element, index)=>{
-        return index!==noteId;
+  async function deleteNote(noteId) {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: 'DELETE'
+    });
+    setNotesArray((prevArray) => {
+      return prevArray.filter((element) => {
+        return element.id !== noteId;
       });
     });
   }
@@ -28,11 +52,23 @@ function App() {
   return (
     <div>
       <Header />
-      <CreateArea addFunction={addNote}/>
-      {notesArray.map((note, index) => (
-        <Note key={index} id={index} title={note.title} content={note.content} clickFunction={deleteNote}/>
-      ))}
-      <Footer />
+      <div className={notesArray.length > 0 ? "" : "hints"}>
+        <CreateArea addFunction={addNote} />
+        {notesArray.length > 0 ? (
+          notesArray.map((note, index) => (
+            <Note
+              key={note.id}
+              id={note.id}
+              title={note.title}
+              content={note.content}
+              clickFunction={deleteNote}
+            />
+          ))
+        ) : (
+          <NoTaskHeader />
+        )}
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/NoTaskHeader.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+function NoTaskHeader(){
+   return <h1 className="hints">Add notes here!</h1>; 
+}
+export default NoTaskHeader;
